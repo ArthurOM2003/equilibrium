29c2d91ccafbf1884595daf57fb71bef285b5948
Arthur Magalhães <40269784+ArthurOM2003@users.noreply.github.com>
2025-08-13 10:59:16 -0300

    deu ruim

--- aporte.js
+++ aporte.js
@@ -1,4 +1,13 @@
 // ** SUA CONFIGURAÇÃO DO FIREBASE VAI AQUI **
+const firebaseConfig = {
+  apiKey: "AIzaSyByPGYoW_GPRstNFx5b1D5qa_qT-VoFQQ0",
+  authDomain: "minha-carteira-app-f8658.firebaseapp.com",
+  projectId: "minha-carteira-app-f8658",
+  storageBucket: "minha-carteira-app-f8658.appspot.com",
+  messagingSenderId: "802101066161",
+  appId: "1:802101066161:web:f9d4f591c2f89a54422590"
+};
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
