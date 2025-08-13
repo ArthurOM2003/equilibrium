c52a3d0e7a9da39b5c0d8b46b24f4a1f532e8404
Arthur Magalhães <40269784+ArthurOM2003@users.noreply.github.com>
2025-08-13 19:54:54 -0300

    Feat: Usa template e build command para injetar config

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByPGYoW_GPRstNFx5b1D5qa_qT-VoFQQ0",
+  authDomain: "minha-carteira-app-f8658.firebaseapp.com",
+  projectId: "minha-carteira-app-f8658",
+  storageBucket: "minha-carteira-app-f8658.appspot.com",
+  messagingSenderId: "802101066161",
+  appId: "1:802101066161:web:f9d4f591c2f89a54422590"
+};
---
+++ firebase-config.template.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "__FIREBASE_API_KEY__",
+  authDomain: "__PUBLIC_FIREBASE_AUTH_DOMAIN__",
+  projectId: "__PUBLIC_FIREBASE_PROJECT_ID__",
+  storageBucket: "__PUBLIC_FIREBASE_STORAGE_BUCKET__",
+  messagingSenderId: "__PUBLIC_FIREBASE_MESSAGING_SENDER_ID__",
+  appId: "__PUBLIC_FIREBASE_APP_ID__"
+};
