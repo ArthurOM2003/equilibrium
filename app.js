eb66fa628b1f1f9b1ab05d60e56e948cb865cc36
Arthur Magalhães <40269784+ArthurOM2003@users.noreply.github.com>
2025-08-13 10:45:23 -0300

    disclaimer

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // ** SUA CONFIGURAÇÃO DO FIREBASE VAI AQUI **
-// Função para buscar a configuração do Firebase de forma segura
-async function getFirebaseConfig() {
-  const response = await fetch('/.netlify/functions/getFirebaseConfig');
-  if (!response.ok) {
-    throw new Error('Não foi possível carregar a configuração do Firebase.');
-  }
-  return response.json();
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyByPGYoW_GPRstNFx5b1D5qa_qT-VoFQQ0",
+  authDomain: "minha-carteira-app-f8658.firebaseapp.com",
+  projectId: "minha-carteira-app-f8658",
+  storageBucket: "minha-carteira-app-f8658.appspot.com",
+  messagingSenderId: "802101066161",
+  appId: "1:802101066161:web:f9d4f591c2f89a54422590"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 
 auth.onAuthStateChanged(user => {
     if (user) {
